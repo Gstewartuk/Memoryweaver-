1d2d515d8db1742e1972361c706e2232c0fc14cc
Gstewartuk <125144266+Gstewartuk@users.noreply.github.com>
2025-12-22 21:33:17 +0000

    Simplify MyApp component by removing auth logic
    Removed session management and Supabase authentication logic.

--- app/pages/_app.tsx
+++ app/pages/_app.tsx
@@ -1,18 +1,6 @@
 import "../styles.css";
 import type { AppProps } from "next/app";
-import { useEffect, useState } from "react";
-import { supabaseClient } from "../lib/supabaseClient";
 
-export default function MyApp({ Component, pageProps }: AppProps) {
-  const [session, setSession] = useState<any>(null);
-
-  useEffect(() => {
-    supabaseClient.auth.getSession().then(({ data }) => setSession(data.session));
-    const { data: listener } = supabaseClient.auth.onAuthStateChange((_event, session) => {
-      setSession(session);
-    });
-    return () => listener.subscription.unsubscribe();
-  }, []);
-
-  return <Component {...pageProps} session={session} />;
+export default function App({ Component, pageProps }: AppProps) {
+  return <Component {...pageProps} />;
 }
